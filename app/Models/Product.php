e1d4c393f08b59b0549d8c9a0792d644a73aa036
shahabsotuhiyan <shahabsotuhiyan@gmail.com>
2024-12-14 13:06:42 +0330

    a quick change on product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-   protected  $fillable = ['title', 'description', 'price', 'thumbnail', 'category'];
+   protected $fillable = ['title', 'description', 'price', 'thumbnail', 'category'];
 
    public function category(){
        return $this->belongsTo(Category::class, 'category_id');
