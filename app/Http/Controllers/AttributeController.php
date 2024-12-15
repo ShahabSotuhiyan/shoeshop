46883df3e8737c8132011687bb1f1060d1f1471d
shahabsotuhiyan <shahabsotuhiyan@gmail.com>
2024-12-15 14:33:12 +0330

    added routes for the products table

---
+++ app/Http/Controllers/AttributeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Attribute;
+use Illuminate\Http\Request;
+
+class AttributeController extends Controller
+{
+
+    public function index(){
+
+    }
+    public function store(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
+
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
+
+}
